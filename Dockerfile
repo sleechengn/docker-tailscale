1c9cc7d4ec44ac923a65bd0a1519db63949a6c13
Der.L <sleechengn@hotmail.com>
2025-04-30 20:23:10 +0800

    create update and fix

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
 from ubuntu:jammy
+#APT_CN_UBUNTU_JAMMY
 run apt update
 run apt install -y curl ttyd
 run echo install source && curl -fsSL https://tailscale.com/install.sh | sh
 #install source
-cmd ["-p", "80", "bash"]
-entrypoint ["/usr/bin/ttyd"]
+run echo "#!/usr/bin/bash" > /docker-entrypoint.sh \
+	&& echo "tailscaled $TAILSCALED_ARGS &" >> /docker-entrypoint.sh \
+        && echo "tailscale $TAILSCALE_ARGS &" >> /docker-entrypoint.sh \
+	&& echo "/usr/bin/ttyd -p 80 /usr/bin/bash" >> /docker-entrypoint.sh \
+	&& chmod +x /docker-entrypoint.sh
+cmd []
+entrypoint ["/docker-entrypoint.sh"]
+env TAILSCALED_ARGS="--socket /root/.tailscaled.sock --tun userspace-networking"
+env TAILSCALE_ARGS="--socket /root/.tailscaled.sock up"
--- build-local.sh
+++ build-local.sh
@@ -14,6 +14,15 @@ rm -rf .git
 
 sed -i '1i\# syntax=docker/dockerfile:1.3' Dockerfile
 
+sed -i "/^#APT_CN_UBUNTU_JAMMY.*/i\RUN apt update" Dockerfile
+sed -i "/^#APT_CN_UBUNTU_JAMMY.*/i\RUN apt install -y ca-certificates" Dockerfile
+sed -i '/^#APT_CN_UBUNTU_JAMMY.*/i\RUN mv /etc/apt/sources.list /etc/apt/sources.list.back' Dockerfile
+sed -i '/^#APT_CN_UBUNTU_JAMMY.*/i\RUN echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse" >> /etc/apt/sources.list' Dockerfile
+sed -i '/^#APT_CN_UBUNTU_JAMMY.*/i\RUN echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse" >> /etc/apt/sources.list' Dockerfile
+sed -i '/^#APT_CN_UBUNTU_JAMMY.*/i\RUN echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse" >> /etc/apt/sources.list' Dockerfile
+sed -i '/^#APT_CN_UBUNTU_JAMMY.*/i\RUN echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse" >> /etc/apt/sources.list' Dockerfile
+sed -i '/^#APT_CN_UBUNTU_JAMMY.*/i\RUN apt update' Dockerfile
+
 sed -i '/.*echo install source.*/d' Dockerfile
 sed -i '/.*#install source.*/i\run --mount=type=bind,target=/root/.cache,rw,source=.cache mkdir /opt/tailscale && cp /root/.cache/tailscale* /opt/tailscale && ln -s /opt/tailscale/tailscale /usr/bin/tailscale && ln -s /opt/tailscale/tailscaled /usr/bin/tailscaled' Dockerfile
 
