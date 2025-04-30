77192586a6adb06603fd19dc8862a67e63faa0a5
root <root@lan.dockerd>
2025-05-01 07:56:33 +0800

    create update and fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 tailscaled --socket $TAILSCALED_SOCKET $TAILSCALED_ARGS &
+
+ls -la $TAILSCALED_SOCKET
+while [ ! -e "$TAILSCALED_SOCKET" ]
+do
+	echo "waiting 1 second for tailscaled boot"
+	sleep 1
+done
+ls -la $TAILSCALED_SOCKET
+
 tailscale --socket $TAILSCALED_SOCKET $TAILSCALE_ARGS &
 tailscale --socket $TAILSCALED_SOCKET set $TAILSCALE_SET &
 
